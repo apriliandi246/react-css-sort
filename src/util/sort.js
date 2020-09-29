e3858bad2adc74caaccb550c29be8c7e5ecee91f
parhan <apriliandi642@gmail.com>
2020-09-29 22:36:43 +0700

    refactor util sort code

--- src/util/sort.js
+++ src/util/sort.js
@@ -1,39 +1,56 @@
-export function sortProperties(properties, { smallerToBigger, biggerToSmaller }) {
+function minSort(properties) {
    let index;
    let current;
    const arrProperties = [...properties];
 
-   if (smallerToBigger === true) {
-      for (let i = 0; i < arrProperties.length; i++) {
-         index = i;
+   for (let i = 0; i < arrProperties.length; i++) {
+      index = i;
 
-         for (let j = i; j < arrProperties.length; j++) {
-            if (arrProperties[j].trim().length < arrProperties[index].trim().length) {
-               index = j;
-            }
+      for (let j = i; j < arrProperties.length; j++) {
+         if (arrProperties[j].toString().trim().length < arrProperties[index].toString().trim().length) {
+            index = j;
          }
-
-         current = arrProperties[i];
-         arrProperties[i] = arrProperties[index];
-         arrProperties[index] = current;
       }
+
+      current = arrProperties[i].toString().trim();
+      arrProperties[i] = arrProperties[index].toString().trim();
+      arrProperties[index] = current;
+   }
+
+   return arrProperties;
+}
+
+function maxSort(properties) {
+   let index;
+   let current;
+   const arrProperties = [...properties];
+
+   for (let i = 0; i < arrProperties.length; i++) {
+      index = i;
+
+      for (let j = i; j < arrProperties.length; j++) {
+         if (arrProperties[j].toString().trim().length > arrProperties[index].toString().trim().length) {
+            index = j;
+         }
+      }
+
+      current = arrProperties[i].toString().trim();
+      arrProperties[i] = arrProperties[index].toString().trim();
+      arrProperties[index] = current;
+   }
+
+   return arrProperties;
+}
+
+
+export function sortProperties(properties, format) {
+   const { smallerToBigger, biggerToSmaller } = format;
+
+   if (smallerToBigger === true) {
+      return minSort(properties).join("\n");
    }
 
    if (biggerToSmaller === true) {
-      for (let i = 0; i < arrProperties.length; i++) {
-         index = i;
-
-         for (let j = i; j < arrProperties.length; j++) {
-            if (arrProperties[j].trim().length > arrProperties[index].trim().length) {
-               index = j;
-            }
-         }
-
-         current = arrProperties[i];
-         arrProperties[i] = arrProperties[index];
-         arrProperties[index] = current;
-      }
+      return maxSort(properties).join("\n");
    }
-
-   return arrProperties.join('\n');
 }
