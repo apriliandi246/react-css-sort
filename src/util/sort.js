c72fb9b903d6c5474a366da19ddf9555566b6113
parhan <apriliandi642@gmail.com>
2020-10-14 14:33:54 +0700

    chnage utility function for sorting css properties

--- src/util/sort.js
+++ src/util/sort.js
@@ -1,56 +1,23 @@
-function minSort(properties) {
-   let index;
-   let current;
-   const arrProperties = [...properties];
-
-   for (let i = 0; i < arrProperties.length; i++) {
-      index = i;
-
-      for (let j = i; j < arrProperties.length; j++) {
-         if (arrProperties[j].toString().trim().length < arrProperties[index].toString().trim().length) {
-            index = j;
-         }
-      }
-
-      current = arrProperties[i].toString().trim();
-      arrProperties[i] = arrProperties[index].toString().trim();
-      arrProperties[index] = current;
-   }
-
-   return arrProperties;
-}
-
-function maxSort(properties) {
-   let index;
-   let current;
-   const arrProperties = [...properties];
-
-   for (let i = 0; i < arrProperties.length; i++) {
-      index = i;
-
-      for (let j = i; j < arrProperties.length; j++) {
-         if (arrProperties[j].toString().trim().length > arrProperties[index].toString().trim().length) {
-            index = j;
-         }
-      }
-
-      current = arrProperties[i].toString().trim();
-      arrProperties[i] = arrProperties[index].toString().trim();
-      arrProperties[index] = current;
-   }
-
-   return arrProperties;
-}
-
-
 export function sortProperties(properties, format) {
    const { smallerToBigger, biggerToSmaller } = format;
 
    if (smallerToBigger === true) {
-      return minSort(properties).join("\n");
+      const result = properties
+         .map((property) => property.toString().trim() + ";")
+         .sort((a, b) => a.length - b.length)
+         .slice(1)
+         .join("\n");
+
+      return result;
    }
 
    if (biggerToSmaller === true) {
-      return maxSort(properties).join("\n");
+      const result = properties
+         .map((property) => property.toString().trim() + ";")
+         .sort((a, b) => b.length - a.length)
+         .slice(0, properties.length - 1)
+         .join("\n");
+
+      return result;
    }
 }
